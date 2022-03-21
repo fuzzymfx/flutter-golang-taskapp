6ccdedc152d97534aaea8d51b2c010b461a4f7ad
Anubhab Patnaik <anubhabr50@gmail.com>
2022-03-21 20:24:49 +0530

    setup get routing

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -4,7 +4,6 @@ import 'package:flutter_golang_yt/colors/app_colors.dart';
 import 'package:flutter_golang_yt/widgets/button_widget.dart';
 import 'package:flutter_golang_yt/widgets/textfield_widget.dart';
 import 'package:get/get.dart';
-
 class AddTask extends StatelessWidget {
   const AddTask({Key? key}) : super(key: key);
 
@@ -32,12 +31,9 @@ class AddTask extends StatelessWidget {
             Column(
               children: [
                 const SizedBox(height: 50,),
-                IconButton(onPressed: (){},
-                    icon: InkWell(
-                      onTap: (){
-                       Get.back();
-                      },
-                        child: const Icon(Icons.arrow_back)),
+                IconButton(onPressed: (){  Get.back();
+                },
+                    icon: const Icon(Icons.arrow_back),
                     color: AppColors.secondaryColor
                 )
 
